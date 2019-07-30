7016952719d839eb227e6a681c7dbb2aafd2a644
gc <gc@dev.null>
2019-07-29 19:38:13 -0500

    added file path agrument

--- go-do/main.go
+++ go-do/main.go
@@ -8,12 +8,42 @@ import (
 
 func main() {
 	args := os.Args[1:]
-	parsed := getInput(args)
-	fmt.Println(parsed[2])
+	parsedTodos := getInput(args)
+	outputPath := getOutputPath(args)
+	output := ""
+	for _, todo := range parsedTodos {
+		output += fmt.Sprintf("%s", makeDo(todo))
+	}
+	fmt.Println(output)
+	exportMd(output, outputPath)
+}
+
+func handleError(e error) {
+	if e!= nil {
+		panic(e)
+	}
 }
 
 func getInput(i []string) []string{
 	parsed := strings.Split(i[0], ",")
 	return parsed
+}
+
+func getOutputPath(i []string) string{
+	return i[1]
+}
+
+func makeDo(todo string) string{
+	return fmt.Sprintf("- [ ] %s \n", strings.Trim(todo, " "))
+}
+
+func exportMd(todos string, path string) {
+	f, err := os.Create(path)
+	handleError(err)
+
+	writtenBytes, err := f.WriteString(todos)
+	handleError(err)
+	fmt.Printf("Wrote %d bytes\n", writtenBytes)
+	f.Sync()
 }
 
